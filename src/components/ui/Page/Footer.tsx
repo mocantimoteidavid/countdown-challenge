b2d72e722c0326502cd74dbb3dbdb98e25828f6a
Timotei Mocan <mocantimoteidavid@gmail.com>
2021-11-16 21:43:20 +0100

    add proper footer

--- src/components/ui/Page/Footer.tsx
+++ src/components/ui/Page/Footer.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components"
 const StyledFooter = styled.footer`
   border-top: 1px solid lightgrey;
   color: grey;
-  padding: 32px 16px;
+  padding: 24px 16px;
   margin-top: 32px;
 `
 
 const Footer = () => (
   <StyledFooter>
-    <p>Developed by Timotei Mocan</p>
+    <p>Made with ❤️ by Timotei Mocan</p>
   </StyledFooter>
 )
 
