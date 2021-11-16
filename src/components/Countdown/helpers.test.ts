f4b70ecfc7bc6602f0f558566f5f0d0c2f2e1389
Timotei Mocan <mocantimoteidavid@gmail.com>
2021-11-16 23:06:43 +0100

    wip diff function test

--- src/components/Countdown/helpers.test.ts
+++ src/components/Countdown/helpers.test.ts
@@ -1,4 +1,9 @@
-import { getSortedFormat, PossibleUnit } from "./helpers"
+// import dayjs from "dayjs"
+import {
+  //   getEndDateDifferenceByUnit,
+  getSortedFormat,
+  PossibleUnit,
+} from "./helpers"
 
 describe("getSortedFormat", () => {
   it("returns a sorted array of units based on the units weight", () => {
@@ -7,3 +12,17 @@ describe("getSortedFormat", () => {
     expect(getSortedFormat(input)).toEqual(["days", "minutes", "seconds"])
   })
 })
+
+// describe("getEndDateDifferenceByUnit", () => {
+//   it("returns an array of diffs between two dayjs values", () => {
+//     const endDate = dayjs("2024-10-10")
+//     const format: PossibleUnit[] = ["years"]
+
+//     const mockedCurrentDate = new Date(2022, 10, 10)
+//     jest
+//       .spyOn(global, "Date")
+//       .mockImplementationOnce(() => (mockedCurrentDate as unknown) as string)
+
+//     expect(getEndDateDifferenceByUnit(endDate, format)).toEqual([4])
+//   })
+// })
