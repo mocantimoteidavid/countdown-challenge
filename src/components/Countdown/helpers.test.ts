b5bd9f1e4d717af5bce10a0bc404bf1d6dbc04b1
Timotei Mocan <mocantimoteidavid@gmail.com>
2021-11-27 17:09:50 +0100

    add proper tests for helpers

--- src/components/Countdown/helpers.test.ts
+++ src/components/Countdown/helpers.test.ts
@@ -1,28 +1,109 @@
-// import dayjs from "dayjs"
+import dayjs from "dayjs"
 import {
-  //   getEndDateDifferenceByUnit,
+  getCurrentTime,
+  getFormattedTimeDifferences,
   getSortedFormat,
   PossibleUnit,
+  shouldRenderLeadingZero,
 } from "./helpers"
 
 describe("getSortedFormat", () => {
-  it("returns a sorted array of units based on the units weight", () => {
+  it("returns a sorted array of units based on the predefined unit weight", () => {
     const input: PossibleUnit[] = ["seconds", "days", "minutes"]
 
-    expect(getSortedFormat(input)).toEqual(["days", "minutes", "seconds"])
+    const output = getSortedFormat(input)
+
+    expect(output).toEqual(["days", "minutes", "seconds"])
   })
 })
 
-// describe("getEndDateDifferenceByUnit", () => {
-//   it("returns an array of diffs between two dayjs values", () => {
-//     const endDate = dayjs("2024-10-10")
-//     const format: PossibleUnit[] = ["years"]
+describe("getFormattedTimeDifferences", () => {
+  let currentTime = getCurrentTime()
 
-//     const mockedCurrentDate = new Date(2022, 10, 10)
-//     jest
-//       .spyOn(global, "Date")
-//       .mockImplementationOnce(() => (mockedCurrentDate as unknown) as string)
+  beforeEach(() => {
+    currentTime = getCurrentTime()
+  })
 
-//     expect(getEndDateDifferenceByUnit(endDate, format)).toEqual([4])
-//   })
-// })
+  it("returns the time differences between two dates in the desired format", () => {
+    const endDate = dayjs().add(1, "day").add(2, "hour")
+    const format: PossibleUnit[] = ["days", "hours"]
+
+    const output = getFormattedTimeDifferences(currentTime, endDate, format)
+
+    expect(output).toEqual([1, 2])
+  })
+
+  it("returns the time differences for all possible units", () => {
+    const endDate = dayjs()
+      .add(1, "year")
+      .add(2, "month")
+      .add(3, "day")
+      .add(4, "hour")
+      .add(5, "minute")
+      .add(6, "second")
+    const format: PossibleUnit[] = [
+      "years",
+      "months",
+      "days",
+      "hours",
+      "minutes",
+      "seconds",
+    ]
+
+    const output = getFormattedTimeDifferences(currentTime, endDate, format)
+
+    expect(output).toEqual([1, 2, 3, 4, 5, 6])
+  })
+})
+
+describe("shouldRenderLeadingZero", () => {
+  it("returns true for 'hours'", () => {
+    const output = shouldRenderLeadingZero("hours")
+
+    expect(output).toBe(true)
+  })
+
+  it("returns true for 'minutes'", () => {
+    const output = shouldRenderLeadingZero("minutes")
+
+    expect(output).toBe(true)
+  })
+
+  it("returns true for 'seconds'", () => {
+    const output = shouldRenderLeadingZero("seconds")
+
+    expect(output).toBe(true)
+  })
+
+  it("returns false for 'years'", () => {
+    const output = shouldRenderLeadingZero("years")
+
+    expect(output).toBe(false)
+  })
+
+  it("returns false for 'months'", () => {
+    const output = shouldRenderLeadingZero("months")
+
+    expect(output).toBe(false)
+  })
+
+  it("returns false for 'days'", () => {
+    const output = shouldRenderLeadingZero("days")
+
+    expect(output).toBe(false)
+  })
+})
+
+describe("getCurrentTime", () => {
+  it("returns correct current time", () => {
+    const fixedTime = 1466424490000
+    const mockedDate = new Date(fixedTime)
+    const spy = jest
+      .spyOn(global, "Date")
+      .mockImplementation(() => (mockedDate as unknown) as string)
+
+    expect(getCurrentTime()).toEqual(1466424490000)
+
+    spy.mockRestore()
+  })
+})
